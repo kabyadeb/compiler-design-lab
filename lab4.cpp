893e4b0a1913dc54f8cdb79487215f67324f8718
kabya deb <98475475+kabyadeb@users.noreply.github.com>
2024-12-03 05:59:42 +0600

    solved 4 no.
    file path maybe changeable

---
+++ lab4.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <string>
+using namespace std;
+void solve()
+{
+
+    string input;
+
+    if (!freopen("/home/mylove/cpp code/text.txt", "r", stdin))
+    {
+        cout << "Error: Unable to open file." << endl;
+        return;
+    }
+    while (getline(cin, input))
+    {
+        cout << "Input line is :" << input << '\n';
+        if (((input[0] >= 'i' && input[0] <= 'n') || (input[0] >= 'I' && input[0] <= 'N')))
+        {
+            for (int i = 1; i < (int)input.size(); i++)
+            {
+                if ((input[i] >= 'a' && input[i] <= 'z') || (input[i] >= 'A' && input[i] <= 'Z') || isspace(input[i]) || (input[i] >= '0' && input[i] <= '9'))
+                    continue;
+                else
+                {
+                    cout << "INvalid input\n";
+                }
+            }
+        }
+        else if (input[0] >= '0' && input[0] <= '9')
+        {
+            if (input.size() < 5)
+            {
+                cout << "ShortInt Number\n";
+            }
+            else if (input.size() > 4)
+            {
+                cout << "LongInt Number\n";
+            }
+            else
+                cout << "INvalid input\n";
+        }
+    }
+}
+int main()
+{
+    solve();
+}
